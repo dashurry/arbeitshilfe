b4b50cce65d4512e5add017f825c88009feb9ec2
dashurry <45306301+dashurry@users.noreply.github.com>
2023-02-11 12:27:34 +0100

    Documenting the Chat App

--- resources/js/components/chat/Container.vue
+++ resources/js/components/chat/Container.vue
@@ -13,20 +13,29 @@
                                     <input type="text" class="form-control" placeholder="Search...">
                                 </div>
                                 <ul class="list-unstyled chat-list mt-2 mb-0">
+
                                     <!-- Iterate over each conversation in the conversations array -->
                                     <router-link v-for="(conv, i) in conversations" :key="i" :to="{name: 'conv-thread', params: {id: conv.id}}">
+
                                         <!-- Add a click event listener to the <li> element -->
                                         <!-- Add a class of "active" if the current route's id parameter matches the id of the conversation -->
                                         <li @click="setActive(conv.id)" class="clearfix" :class="{ 'active': $route.params.id === conv.id }">
+
                                             <!-- If the conversation's participant has a thumbnail image, display it. Otherwise, display a div element
                                             with a class of "user-thumb-alt" and a background color specified by conv.participant.thumb_color -->
                                             <div v-if="conv.participant.thumb == null" class="user-thumb-alt" :style="{'background-color' : conv.participant.thumb_color}">{{ conv.participant.thumb_alter }}</div>
                                             <img :src="conv.participant.thumb" :alt="conv.participant.thumb_alter">
+
                                             <div class="about">
                                                 <!-- Display the conversation's participant's name -->
                                                 <div class="name">{{ conv.participant.name }}</div>
+
+                                                <!-- If the participant is online, then display the online status. -->
                                                 <div v-if="conv.participant.is_online" class="status"> <i class="fa fa-circle online"></i> online </div>
+
+                                                <!-- If the participant is offline, then display the offline status. -->
                                                 <div v-else class="status"> <i class="fa fa-circle offline"></i>
+
                                                 <!-- Display the time that the conversation's last message was created using the vue-moments-ago component -->
                                                 <vue-moments-ago :key="i" prefix="" suffix="ago" :date="conv.last_msg.created_at" lang="en"></vue-moments-ago>
                                                 </div>                                            
@@ -36,6 +45,7 @@
                                 </ul>
                             </div>
                         </template>
+                        <!-- Use the router-view component -->
                         <router-view :key="$route.fullPath"></router-view>                     
                     </div>
                 </div>
@@ -53,20 +63,29 @@ export default {
     },
     data(){
         return {
+            // This is the thread details object
             threadDetails: {},
+            // This is the variable to show the loading message
             loadingMsg: true,
+            // This is the variable to show the loading conversation
             loadingConv: true,
+            // This is the user id that is currently logged in
             user_id: USERID,
+            // This is the boolean to show the people list
             showPeopleList: true,
         }
     },
     created() {
+        // add a reference to the component to the root Vue instance
         this.$root.$refs.CHAT_CONTAINER = this;
     },
     computed : {
-        conversations(){
+        // Define the conversations method
+        conversations() {
+            // Return the value of the "convs" getter from the "Conversation" module in the store
             return this.$store.getters["Conversation/convs"];
         }
+
     },
     methods: {
         // The setActive method uses the $router.push method to update the active route based on the ID of the clicked <li> element.
@@ -75,8 +94,8 @@ export default {
         this.$router.push({ name: 'conv-thread', params: { id } });
         this.showPeopleList = false;
         },
-        toggleActive() {
-            this.showPeopleList = !this.showPeopleList; // Toggle the value of showPeopleList when the button is clicked
+        toggleActive() { // This is the method called when the button is clicked
+            this.showPeopleList = !this.showPeopleList; // Toggle the value of showPeopleList
         }
     }
 }
